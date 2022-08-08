e5446e93826866f4e74d3a056b64d4822ab52020
Nanako <00sh.chiha+github@gmail.com>
2022-08-08 21:40:12 +0900

    loadmodule??

--- test/packed.lua
+++ test/packed.lua
@@ -2,7 +2,6 @@ local proxy_package = {
   loaded = {},
   packages = {}
 }
-local _load_module = load_module
 
 function load_module(h)
   if (proxy_package.loaded[h]) then
@@ -11,26 +10,33 @@ function load_module(h)
     proxy_package.loaded[h] = proxy_package.packages[h]()
     return proxy_package.loaded[h]
   else
-    return _load_module(h)
+    local f, err = loadfile(h)
+    if (f) then
+      proxy_package.packages[h] = f
+      proxy_package.loaded[h] = f()
+      return proxy_package.loaded[h]
+    else
+      return nil, err
+    end
   end
 end
 
 proxy_package.packages['c36e8531'] = function()
-print('Hello world!')
-
-local test2 = load_module('1cd9fb73')
-load_module('1af74717')
-print(test2:add(1,2))
+	print('Hello world!')
+	
+	local test2 = load_module('1cd9fb73')
+	load_module('1af74717')
+	print(test2:add(1,2))
 end
 proxy_package.packages['1cd9fb73'] = function()
-local lib = {}
-function lib:add(a, b)
-    return a + b
-end
-return lib
-
+	local lib = {}
+	function lib:add(a, b)
+	    return a + b
+	end
+	return lib
+	
 end
 proxy_package.packages['1af74717'] = function()
-print('78 from within subdirectories!')
+	print('78 from within subdirectories!')
 end
 load_module('c36e8531')
